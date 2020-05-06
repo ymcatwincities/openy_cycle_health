6c84175925635bf3445a78c062ee2bbeea58e2f5
Anatoliy Polyakov <anpolimus@gmail.com>
2020-05-06 14:03:39 +0300

    Added build script

---
+++ build.sh
@@ -0,0 +1,34 @@
+echo "Getting last repository update";
+
+source 'env.sh'
+
+cd $HOME
+git pull
+
+echo "Building composer packages"
+
+cd $HOME
+COMPOSER_MEMORY_LIMIT=-1 composer install
+
+echo "Importing config and updating database"
+
+cd $HOME
+./vendor/bin/drush cr
+./vendor/bin/drush cim -y
+./vendor/bin/drush updb -y
+
+echo "Building theme"
+
+cd $HOME/docroot/themes/custom/twelve
+npm install
+npm run build
+
+echo "Building apps"
+
+cd $HOME/docroot/modules/custom/twelve_app/bursts
+npm install
+npm run build
+
+cd $HOME/docroot/modules/custom/twelve_app/puzzle
+npm install
+npm run build
