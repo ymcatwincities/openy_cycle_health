84943d5c8067657113b16a86e89d329e52642687
Anatoliy Polyakov <anpolimus@gmail.com>
2020-06-01 16:16:17 +0300

    Added seven summits settings page

---
+++ docroot/modules/custom/twelve_app/src/Form/SevenSummitsSettingsForm.php
@@ -0,0 +1,172 @@
+<?php
+
+namespace Drupal\twelve_app\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\node\Entity\Node;
+
+/**
+ * Settings Form for bingo game.
+ */
+class SevenSummitsSettingsForm extends ConfigFormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'twelve_app_seven_summits_admin_settings';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return [
+      'twelve_app.seven_summits_settings',
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+
+    $config = $this->config('twelve_app.seven_summits_settings');
+
+    if (empty($form_state->get('items'))) {
+      $items = $config->get('items');
+      $form_state->set('items', $items);
+    } else {
+      $items = $form_state->get('items');
+    }
+
+    $form['#tree'] = TRUE;
+    $form_state->setCached(FALSE);
+
+    $form['actions'] = [
+      '#type' => 'actions',
+    ];
+
+    $form['nodes_container'] = array(
+      '#type' => 'fieldset',
+      '#title' => $this->t('Items'),
+      '#prefix' => '<div id="items-wrapper">',
+      '#suffix' => '</div>',
+    );
+
+    $i = 0;
+    foreach ($items as $item) {
+
+      $node = NULL;
+
+      $form['nodes_container'][$i] = [
+        '#type' => 'fieldset',
+        '#title' => 'Schedule Item',
+      ];
+
+      if (!empty($item['node_id'])) {
+        $node = Node::load($item['node_id']);
+      }
+
+      $form['nodes_container'][$i]['node_id'] = [
+        '#type' => 'entity_autocomplete',
+        '#target_type' => 'node',
+        '#title' => t('Mountain Page'),
+        '#default_value' => $node,
+        '#size' => 40,
+        '#selection_settings' => [
+          'target_bundles' => ['landing_page'],
+        ],
+        '#description' => t('Search by title'),
+      ];
+
+      $i++;
+
+    }
+
+    $form['add_item']= [
+      '#type' => 'submit',
+      '#value' => 'Add item',
+      '#submit' => [[$this, 'add_item_submit']],
+      '#ajax' => [
+        'callback' => [$this, 'add_item_callback'],
+        'wrapper' => 'items-wrapper',
+      ],
+    ];
+
+    $form['delete_first'] = [
+      '#type' => 'submit',
+      '#value' => 'Delete item',
+      '#submit' => [[$this, 'delete_item_submit']],
+      '#ajax' => [
+        'callback' => [$this, 'add_item_callback'],
+        'wrapper' => 'items-wrapper',
+      ],
+    ];
+
+
+    return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * @param $form
+   * @param $form_state
+   */
+  public function delete_item_submit(&$form, FormStateInterface $form_state) {
+    $items = $form_state->get('items');
+    if (empty($items[0])) {
+      $items = NULL;
+    } else {
+      $items = array_shift($items);
+      if (empty($items[0])) {
+        $items = [
+          [
+            'node' => '',
+          ]
+        ];
+      }
+    }
+
+    if (empty($items[0])) {
+      $items = [$items];
+    }
+
+    $form_state->set('items', array_values($items));
+    $form_state->setRebuild(TRUE);
+  }
+
+  /**
+   * @param $form
+   * @param $form_state
+   */
+  public function add_item_submit(&$form, FormStateInterface $form_state) {
+    $items = $form_state->get('items');
+    $items[] = [
+      'node_id' => NULL,
+    ];
+    $form_state->set('items', $items);
+    $form_state->setRebuild(TRUE);
+  }
+
+  /**
+   * @param $form
+   * @param $form_state
+   *
+   * @return mixed
+   */
+  public function add_item_callback(&$form, FormStateInterface $form_state) {
+    return $form['nodes_container'];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    /* @var $config \Drupal\Core\Config\Config */
+    $config = $this->configFactory->getEditable('twelve_app.seven_summits_settings');
+    $config->set('items', $form_state->getValue('nodes_container'))->save();
+    parent::submitForm($form, $form_state);
+  }
+
+}
