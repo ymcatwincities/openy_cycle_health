22e4e938a8dd1181c1a5fda59f0e29995f2edef4
Oleksandr Kuksenko <oleksandrk@imagexmedia.com>
2020-06-19 22:10:23 +0300

    greeting block styles

---
+++ docroot/modules/custom/twelve_app/js/seven_summits/components/Greeting.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="greetings-block-wrapper">
+    <div class="greeting-block">
+      <div class="climber-wrapper">
+        <Climber :class="['hero', $store.state.jacketColor, $store.state.fleshTone]"></Climber>
+      </div>
+      <div class="user-name">Hi, {{ username }}</div>
+      <div class="edit" v-on:click="editCharacterClick"></div>
+    </div>
+  </div>
+</template>
+<script>
+  import { mapMutations } from "vuex";
+  import Climber from "./Climber";
+  import twelve from "../../helper/twelve";
+
+  export default {
+    components: { Climber },
+    computed: {
+      username() {
+        return twelve.user.get_active_player_name();
+      }
+    },
+    methods: {
+      ...mapMutations([
+        'updateHeroSelected'
+      ]),
+      editCharacterClick: function() {
+        this.updateHeroSelected();
+        this.$router.push({ name: "Hero"});
+      }
+    },
+  }
+</script>
--- docroot/modules/custom/twelve_app/js/seven_summits/views/Mountains.vue
+++ docroot/modules/custom/twelve_app/js/seven_summits/views/Mountains.vue
@@ -1,15 +1,6 @@
 <template>
-
   <div class="views-layout">
-    <div class="greetings-block-wrapper">
-    <div class="greeting-block">
-      <div class="climber-wrapper">
-        <Climber :class="['hero', $store.state.jacketColor, $store.state.fleshTone]"></Climber>
-      </div>
-      <div class="user-name">Hi, {{ username }}</div>
-      <div class="edit" v-on:click="editCharacterClick"></div>
-    </div>
-    </div>
+    <Greeting></Greeting>
     <div class="mountains-layout">
       <Mountain
         v-for="summit in summits"
@@ -20,34 +11,19 @@
 </template>
 
 <script>
+  import { mapState } from 'vuex';
   import Mountain from "../components/Mountain";
-  import Climber from "../components/Climber";
-  import { mapState, mapMutations } from 'vuex';
-  import twelve from "../../helper/twelve";
+  import Greeting from "../components/Greeting";
 
   export default {
-    data: function() {
-      return {
-        username: twelve.user.get_active_player_name(),
-      }
-    },
     components: {
+      Greeting,
       Mountain,
-      Climber
     },
     computed: {
       ...mapState([
         'summits'
       ]),
     },
-    methods: {
-      ...mapMutations([
-        'updateHeroSelected'
-      ]),
-      editCharacterClick: function() {
-        this.updateHeroSelected();
-        this.$router.push({ name: "Hero"});
-      }
-    }
   }
 </script>
